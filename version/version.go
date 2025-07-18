c1bee42bd58bbc1a9c163fad3356a846b317b679
Jason Madigan <jason@jasonmadigan.com>
2025-07-18 19:18:56 +0100

    Bump version to 0.1.0
    Major architectural improvements warrant minor version bump

--- version/version.go
+++ version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Variables initialized by the Go linker at build time - see Makefile
 
-var Version = "0.0.8"
+var Version = "0.1.0"
 var SetupImage = "quay.io/jmadigan/podman-mac-net-connect"
